721ddebfe5f22b55c65bdce2e6fc0db39b539457
Lucas Azevedo <lucasazevedominas@gmail.com>
2026-09-20 22:13:42 -0300

    rewrite the app logic with async/await concepts

--- script.js
+++ script.js
@@ -2,22 +2,33 @@ const img = document.querySelector("img");
 const input = document.querySelector("input");
 const button = document.querySelector("button");
 
-button.addEventListener("click", () => {
-  let value = input.value;
+// button.addEventListener("click", () => {
+//   let value = input.value;
   
-  if(!value) {
-    alert("Please enter your search.")
+//   if(!value) {
+//     alert("Please enter your search.")
+//   }
+//   else {
+//     fetch(`https://api.giphy.com/v1/gifs/translate?api_key=enZmTDAKShhj7smXYDZHETmNrOxTJfcv&s=${value}&rating=g`)
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(response) {
+//       img.src = response.data.images.original.url;
+//     })
+//     .catch(error => {
+//       alert(error);
+//     })
+//   }
+// });
+
+async function getGif() {
+  try {
+    const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=enZmTDAKShhj7smXYDZHETmNrOxTJfcv&s=cats&rating=g`);
+    const gifData = await response.json();
+    img.src = gifData.data.images.original.url;
+  } 
+  catch(error) {
+    console.error(error);
   }
-  else {
-    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=Argk3PNvMifazs1LkceMMw3Lvrs4VknH&s=${value}&rating=g`)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(response) {
-      img.src = response.data.images.original.url;
-    })
-    .catch(error => {
-      alert(error);
-    })
-  }
-});
+}
