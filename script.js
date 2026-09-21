4d00cb0df4993e8acbacacc4eeab0d1bca0b7c7a
Lucas Azevedo <lucasazevedominas@gmail.com>
2026-09-20 22:16:37 -0300

    refactor button event

--- script.js
+++ script.js
@@ -2,29 +2,10 @@ const img = document.querySelector("img");
 const input = document.querySelector("input");
 const button = document.querySelector("button");
 
-// button.addEventListener("click", () => {
-//   let value = input.value;
-  
-//   if(!value) {
-//     alert("Please enter your search.")
-//   }
-//   else {
-//     fetch(`https://api.giphy.com/v1/gifs/translate?api_key=enZmTDAKShhj7smXYDZHETmNrOxTJfcv&s=${value}&rating=g`)
-//     .then(function(response) {
-//       return response.json();
-//     })
-//     .then(function(response) {
-//       img.src = response.data.images.original.url;
-//     })
-//     .catch(error => {
-//       alert(error);
-//     })
-//   }
-// });
 
-async function getGif() {
+async function getGif(value) {
   try {
-    const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=enZmTDAKShhj7smXYDZHETmNrOxTJfcv&s=cats&rating=g`);
+    const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=enZmTDAKShhj7smXYDZHETmNrOxTJfcv&s=${value}&rating=g`);
     const gifData = await response.json();
     img.src = gifData.data.images.original.url;
   } 
@@ -32,3 +13,8 @@ async function getGif() {
     console.error(error);
   }
 }
+
+button.addEventListener("click", () => {
+  let value = input.value;
+  getGif(value);
+});
