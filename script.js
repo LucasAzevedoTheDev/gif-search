cf62765dabf028116fe8754d289f9b857def824a
Lucas Azevedo <lucasazevedominas@gmail.com>
2026-09-20 21:08:41 -0300

    add button event listener

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ const img = document.querySelector("img");
 const input = document.querySelector("input");
 const button = document.querySelector("button");
 
+button.addEventListener("click", () => {
+  let value = input.value;
+  
+  if(!value) {
+    alert("Please enter your search.")
+  }
+  else {
+    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=Argk3PNvMifazs1LkceMMw3Lvrs4VknH&s=${value}&rating=g`)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+      img.src = response.data.images.original.url;
+    })
+    .catch(error => {
+      alert(error);
+    })
+  }
+});
