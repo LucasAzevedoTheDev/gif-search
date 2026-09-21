2513c391b28abffbd6e45736eb1f6e550c57deb2
Lucas Azevedo <lucasazevedominas@gmail.com>
2026-09-20 23:39:59 -0300

    modify the img appending

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const img = document.querySelector("img");
+const img = document.createElement("img");
+const imgContainer = document.querySelector(".image-container");
 const input = document.querySelector("input");
 const button = document.querySelector("button");
 
@@ -6,6 +7,7 @@ async function getGif(value) {
   try {
     const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=enZmTDAKShhj7smXYDZHETmNrOxTJfcv&s=${value}&rating=g`);
     const gifData = await response.json();
+    imgContainer.appendChild(img);
     img.src = gifData.data.images.original.url;
   } 
   catch(error) {
